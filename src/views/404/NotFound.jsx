34005975b26dadd201e3b682909f0567156173da
shanming.wang <wangshanming@zhongshuruizhi.com>
2025-01-24 17:25:39 +0800

    feat: project init finished

--- src/views/404/NotFound.jsx
+++ src/views/404/NotFound.jsx
@@ -1,8 +1,11 @@
+import { Result } from 'antd';
 
 export default function NotFound() {
   return (
-    <div>
-      404
-    </div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+    />
   );
 }
--- src/views/setting/SettingIndex.jsx
+++ src/views/setting/SettingIndex.jsx
@@ -1,16 +1,33 @@
+import { App, Space, Button } from 'antd';
+import homePng from '@/assets/icon/home.png';
+import homeSvg from '@/assets/icon/home.svg';
+
 export default function SettingIndex() {
+  const { message } = App.useApp();
+
+  function handleMessageShow(type) {
+    message[type]('This is a message');
+  }
+
   return (
     <div>
-      <div className="mt-4">
+      <Space>
+        <Button onClick={() => handleMessageShow('info')}>Message</Button>
+        <Button variant="solid" color="green" onClick={() => handleMessageShow('success')}>Message</Button>
+        <Button variant="solid" color="yellow" onClick={() => handleMessageShow('warning')}>Message</Button>
+        <Button variant="solid" color="danger" onClick={() => handleMessageShow('error')}>Message</Button>
+      </Space>
+
+      <div style={{ marginTop: 24 }}>
         <img
           className="img-home"
-          src="@/assets/icon/home.svg"
+          src={homeSvg}
           alt="home"
           title="img-svg"
         />
         <img
           className="img-home"
-          src="@/assets/icon/home.png"
+          src={homePng}
           alt="home"
           title="img-png"
         />
